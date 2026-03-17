0e2bce02561a0f348d5c08fc9b33d6fa948cc2cf
Toan Dang <thanhtoan06092004@gmail.com>
2026-03-17 10:43:23 +0700

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,84 @@
+var express = require("express");
+var router = express.Router();
+let userController = require('../controllers/users')
+let { RegisterValidator, ChangePasswordValidator, validationResult } = require('../utils/validatorHandler')
+let { CheckLogin } = require('../utils/authHandler')
+let jwt = require('jsonwebtoken')
+let fs = require('fs')
+let path = require('path')
+
+let privateKey = fs.readFileSync(path.join(__dirname, '../private.key'), 'utf8');
+let publicKey = fs.readFileSync(path.join(__dirname, '../public.key'), 'utf8');
+
+router.post('/register', RegisterValidator, validationResult, async function (req, res, next) {
+    try {
+        let newItem = await userController.CreateAnUser(
+            req.body.username, req.body.password, req.body.email,
+            "69af870aaa71c433fa8dda8e"
+        )
+        res.send(newItem);
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
+})
+router.post('/login', async function (req, res, next) {
+    try {
+        let { username, password } = req.body;
+        let result = await userController.FindUserByUsername(username);
+        if (!result) {
+            res.status(403).send("sai thong tin dang nhap");
+            return;
+        }
+        if (result.lockTime > Date.now()) {
+            res.status(404).send("ban dang bi ban");
+            return;
+        }
+        result = await userController.CompareLogin(result, password);
+        if (!result) {
+            res.status(403).send("sai thong tin dang nhap");
+            return;
+        }
+        let token = jwt.sign({
+            id: result._id
+        }, privateKey, {
+            expiresIn: '1d',
+            algorithm: 'RS256'
+        })
+        res.cookie("LOGIN_NNPTUD_S3", token, {
+            maxAge: 24 * 60 * 60 * 1000,
+            httpOnly: true
+        })
+        res.send(token)
+
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
+})
+router.get('/me', CheckLogin, function (req, res, next) {
+    let user = req.user;
+    res.send(user)
+})
+router.post('/logout', CheckLogin, function (req, res, next) {
+    res.cookie("LOGIN_NNPTUD_S3", "", {
+        maxAge: 0,
+        httpOnly: true
+    })
+    res.send("da logout ")
+})
+
+router.post('/changepassword', CheckLogin, ChangePasswordValidator, validationResult, async function (req, res, next) {
+    try {
+        let user = req.user;
+        let { oldPassword, newPassword } = req.body;
+        let isMatch = await userController.CompareLogin(user, oldPassword);
+        if (!isMatch) {
+            return res.status(403).send({ message: "Mat khau cu khong chinh xac" });
+        }
+        await userController.ChangePassword(user, newPassword);
+        res.send({ message: "Doi mat khau thanh cong" });
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
+})
+
+module.exports = router;
---
+++ utils/authHandler.js
@@ -0,0 +1,40 @@
+let userController = require('../controllers/users')
+let jwt = require('jsonwebtoken')
+let fs = require('fs')
+let path = require('path')
+
+let publicKey = fs.readFileSync(path.join(__dirname, '../public.key'), 'utf8');
+
+module.exports = {
+    CheckLogin: async function (req, res, next) {
+        let key = req.headers.authorization;
+        if (!key) {
+            if (req.cookies.LOGIN_NNPTUD_S3) {
+                key = req.cookies.LOGIN_NNPTUD_S3;
+            } else {
+                res.status(404).send("ban chua dang nhap")
+                return;
+            }
+
+        }
+
+        try {
+            let result = jwt.verify(key, publicKey, { algorithms: ['RS256'] })
+            if (result.exp * 1000 < Date.now()) {
+                res.status(404).send("ban chua dang nhap")
+                return;
+            }
+            let user = await userController.GetUserById(result.id);
+            if (!user) {
+                res.status(404).send("ban chua dang nhap")
+                return;
+            }
+            req.user = user;
+            next();
+        } catch (error) {
+            res.status(404).send("ban chua dang nhap")
+            return;
+        }
+
+    }
+}
